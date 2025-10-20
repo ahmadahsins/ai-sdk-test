bd7dfcd2c72d291850c7a7ddf6b227a0e1a1a9fb
Ahmad Ahsin Syamil <118179124+ahmadahsins@users.noreply.github.com>
2025-10-20 20:48:16 +0700

    feat: add multi modal chat (analyze image and pdf)

---
+++ src/app/api/multi-modal-chat/route.ts
@@ -0,0 +1,18 @@
+import { groq } from "@ai-sdk/groq";
+import { convertToModelMessages, streamText, UIMessage } from "ai";
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+
+    const result = streamText({
+      model: groq("meta-llama/llama-4-maverick-17b-128e-instruct"),
+      messages: convertToModelMessages(messages),
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error("Error streaming chat completion:", error);
+    return new Response("Failed to stream chat completion", { status: 500 });
+  }
+}
